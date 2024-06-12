cec2a2d0f3304d1c4756e4f594d8aa66f2881635
rugatling <dr.dudochkin@bk.ru>
2024-06-12 17:14:29 +0300

    struct
    struct

--- chipper/pkg/wirepod/preqs/knowledgegraph.go
+++ chipper/pkg/wirepod/preqs/knowledgegraph.go
@@ -190,7 +190,7 @@ func openaiRequest(transcribedText string) string {
 		logger.Println(string(body))
 		return "Open proxyAI returned no response."
 	}
-	apiResponse := strings.TrimSpace(openAIResponse.Choices[0].Text)
+	apiResponse := strings.TrimSpace(openAIResponse.message[0].Text)
 	logger.Println("OpenproxyAI response: " + apiResponse)
 	return apiResponse
 }
