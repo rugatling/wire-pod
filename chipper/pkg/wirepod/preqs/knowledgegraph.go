872c0edb5580194b94c686a37043b9e2bc63a49d
rugatling <dr.dudochkin@bk.ru>
2024-06-12 19:21:05 +0300

    1
    1

--- chipper/pkg/wirepod/preqs/knowledgegraph.go
+++ chipper/pkg/wirepod/preqs/knowledgegraph.go
@@ -197,7 +197,7 @@ func openaiRequest(transcribedText string) string {
 		logger.Println(string(body))
 		return "Open proxyAI returned no response."
 	}
-	apiResponse := strings.TrimSpace(openAIResponse.Choices[0].Content)
+	apiResponse := strings.TrimSpace(openAIResponse.Choice[0].Message.Content)
 	logger.Println("OpenproxyAI response: " + apiResponse)
 	return apiResponse
 }
